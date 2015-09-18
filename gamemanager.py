4e7c2d2c44245e82c3d962e3692a6e480d38a8c7
nmc16 <ilovehockey95@hotmail.com>
2015-09-18 18:24:38 +0000

    Added piface code

--- gamemanager.py
+++ gamemanager.py
@@ -1,6 +1,7 @@
 import sys
+import pifacedigitalio as pfio
 from time import *
-
+from threading import * 
 
 class GameManager(object):
 
@@ -11,6 +12,7 @@ class GameManager(object):
         # Reset the score and player name
         self.highscore = 0
         self.playername = playername
+        self.pfd = pfio.PiFaceDigital()
 
     def init_game(self):
         # Print the menu
@@ -33,13 +35,33 @@ class GameManager(object):
         print ""
 
     def run_gamemanager(self):
+        t1_stop = Event()
+        t1 = Thread(target=self.run_leds, args=(1, t1_stop))
         while(True):
             command = raw_input("> ")
             if command in self.commands:
-                print "> ", command, " registered"
+                if command == "start":
+                   t1_stop = Event()
+                   t1 = Thread(target=self.run_leds, args=(1, t1_stop))
+                   t1.start()
+                if command == "stop":
+                    t1_stop.set()
             else:
                 print "> Command does not exist. Type help to see all available commands!"
-
+    
+    def run_leds(self, arg1, stop_event):
+        i = 0  
+        while(not stop_event.is_set()):
+            sleep(1)
+            self.pfd.leds[i].turn_on()
+            if i > 0:
+                self.pfd.leds[i-1].turn_off()
+            else:
+                self.pfd.leds[7].turn_off()
+            if i < 7:
+                i = i + 1
+            else:
+                i = 0 
 
 def main():
     gm = GameManager(sys.argv[1])
